04f35a174f4183c0d219fdee5cbaf165d375b782
Alan Arturo Pedraza Luna <alanbanano@gmail.com>
2022-05-23 02:41:10 -0500

    MySeries Changes Apply

--- src/views/MySeries.vue
+++ src/views/MySeries.vue
@@ -1,26 +1,65 @@
-<script setup>
+<script>
 import ContinuarViendo from '../components/ContinuarViendo.vue';
+import { userStore } from "../stores/user";
+import axios from "axios";
+
+export default {
+
+    setup() {
+        const store = userStore();
+        // Make it available inside methods
+        return { store }
+    },
+
+    data() {
+        return {
+            is_ready: false,
+            series: {}
+        }
+    },
+
+    methods: {
+        getMySeries() {
+            axios.get("http://localhost:8000/series/userSeries/" + this.store.user_id)
+                .then(response => {
+                    console.log(response.data)
+                    this.series = response.data
+                    this.is_ready = true
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        getSeriesDetail(id_serie){
+            this.$router.push("/series/" + id_serie)
+        }
+    },
+
+    mounted() {
+        this.getMySeries()
+    }
+
+}
+
 
 </script>
 
 
 <template>
-    <div class="dashboard">
-        <ContinuarViendo/>
+    <div v-if="is_ready" class="dashboard">
+        <ContinuarViendo />
         <i class="bi bi-star-fill mx-2 text-warning align-middle"></i>
         <span class="h4 align-middle">Mis Series</span>
 
-        <div class=" d-flex flex-row justify-content-between align-content-around flex-wrap">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D1.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D2.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D3.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D4.png" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D1.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D2.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D3.jpg" alt="">
-            <img class="rounded w-25 h-25 mt-4" src="../assets/D4.png" alt="">
+        <div class="d-flex flex-row justify-content-between align-content-around flex-wrap">
+            <img v-for="serie in series" class="rounded w-25 h-25 mt-4" :src="serie.imagen" :alt="serie.nombre_serie"
+                :key="serie.id_serie" @click="getSeriesDetail(serie.id_serie)">
         </div>
     </div>
+    <div class="spinner-border text-primary" role="status" v-else>
+        <span class="visually-hidden">Loading...</span>
+    </div>
+
 
 </template>
 
@@ -35,5 +74,4 @@ import ContinuarViendo from '../components/ContinuarViendo.vue';
 .w-25 {
     width: 23% !important;
 }
-
 </style>
--- src/views/SerieDetail.vue
+++ src/views/SerieDetail.vue
@@ -1,18 +1,86 @@
 <script>
+import { seriesStore } from "../stores/series";
+import { userStore } from "../stores/user";
+import { useToast } from "vue-toastification";
+import axios from "axios";
 
+export default {
 
+    setup() {
+        const store = seriesStore();
+        const toast = useToast();
+        const store2 = userStore();
+
+        return { store, store2, toast }
+    },
+
+    data() {
+        return {
+            series: {},
+            is_ready: false,
+            serie: {},
+            imagen: String
+        }
+    },
+
+    methods: {
+        setFilter(value) {
+            this.filter_value = value
+        },
+
+        getSeriesDetail(id_serie) {
+            this.$router.push("/series/" + id_serie)
+        },
+
+        addSeriesUser(){
+            let new_serie = {
+                "id_usuario": this.store2.user_id,
+                "id_serie": this.$route.params.id,
+                "imagen": this.imagen,
+            }
+            console.log(new_serie)
+            axios.post("http://localhost:8000/series/userSeries", new_serie)
+            .then(response => {
+                console.log(response.data)
+                
+                let { userSeries_added } = response.data
+
+                if(userSeries_added){
+                  this.toast.success("Se agrego la Serie Correctamente")
+                }
+                else{
+                  this.toast.error("Error al Agregar la Serie")
+                }
+            })
+            .catch(error =>{
+              console.log(error)
+              this.toast.error("Ya tienes Esta Serie Agregada")
+            })
+        }
+    },
+
+    mounted() {
+        this.series = this.store.getSeries()
+        this.is_ready = true
+        this.serie = this.series.filter(serie => serie.id_serie == this.$route.params.id)
+        this.serie.map(serie =>{
+            this.imagen = serie.imagen
+        })
+        
+    },
+
+}
 </script>
 
 
 <template>
-    <div class="dashboard">
-        <p>{{ $route.params.id }}</p>
-        <div class="row ">
+    <div v-if="is_ready" v-for="serie in serie" class="dashboard">
+        <div class="row">
             <div class="col">
-                <h4 class="mb-4">{Nombre Serie}</h4>
+                <h4 class="mb-4">{{ serie.nombre_serie }}</h4>
             </div>
             <div class="col d-flex justify-content-end h-75">
-                <button class="btn btn-success me-3">
+                <button class="btn btn-success me-3" @click="addSeriesUser()">
                     <i class="bi bi-star-fill text-warning me-2"></i>Agregar a Mis Series
                 </button>
                 <button class="btn btn-success">
@@ -24,24 +92,23 @@
         <i class="bi bi-star-fill text-warning align-middle"></i>
         <i class="bi bi-star-fill text-warning align-middle"></i>
         <i class="bi bi-star-fill text-warning align-middle"></i>
-        <i class="bi bi-star-fill text-warning align-middle me-2"></i>
-        <span class="h5 align-middle">Calificación</span>
+        <i class="bi bi-star-fill text-warning align-middle"></i>
+        <span class="h5 ms-2 align-middle">Calificación</span>
 
-        <p class="mt-4 h6"><b>Género:</b> {Genero Serie}</p>
+        <p class="mt-4 h6"><b>Género: </b>{{ serie.genero }}</p>
 
         <div class="row mt-5">
             <div class="col-5 d-flex align-items-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce odio libero, dignissim at mauris id,
-                venenatis tempus metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget elit lectus.
-                Integer non ultrices purus. Maecenas efficitur tellus ipsum, ut commodo sapien convallis ut. Vivamus
-                urna massa, semper quis velit gravida, hendrerit tempus diam. Mauris faucibus ligula sit amet sapien
-                scelerisque blandit.
+                {{ serie.descripcion }}
             </div>
             <div class="col-7">
-                <img class="img-fluid" src="../assets/D1.jpg" alt="">
+                <img class="img-fluid" :src="serie.imagen" alt="">
             </div>
         </div>
 
+    </div>
+    <div v-else class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
     </div>
 
 </template>
@@ -49,7 +116,7 @@
 
 <style scoped>
 .dashboard {
-    margin-top: 3rem;
+    margin-top: 6rem;
     margin-left: 3rem;
     max-width: 70%;
 }
